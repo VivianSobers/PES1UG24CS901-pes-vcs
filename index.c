310a4150f0d64d5409ee14c4aa528189742771cc
Vivian Sobers E <vivianedwardbangalore@gmail.com>
2026-04-20 11:11:58 +0530

    Phase 3: implement index load, save and staging area

--- index.c
+++ index.c
@@ -23,7 +23,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
-
+ 
 // Forward declarations from object.c
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
 
